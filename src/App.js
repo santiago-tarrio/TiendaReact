a5b8360f62d0f7552018638f635060253fb308a9
Santiago Tarrio <tarrio.sant@gmail.com>
2022-05-03 15:12:21 -0300

    agregado navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import NavBar from "./components/Navbar/Navbar"
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar />
         <h1>Tienda</h1>
       </header>
     </div>
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+export default function Navbar (){
+    
+    return (
+        <header className="header">
+            <div className="containerLogo">
+                <p>img del logo</p>
+            </div>
+            <ul className="linkList">
+                <li>Inicio</li>
+                <li>Pagina1</li>
+                <li>Pagina2</li>
+            </ul>
+        </header>
+    );
+
+}
