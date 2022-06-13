f2a9313c4ebc27f89d72481339da6c64b08cd380
Santiago Tarrio <tarrio.sant@gmail.com>
2022-06-13 16:02:11 -0300

    firebase 3

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,28 +1,25 @@
 import React, { useState, useEffect } from "react";
 import ItemList from "../ItemList/ItemList";
-import {getFirestore, getDocs, collection} from "firebase/firestore";
+import {getFirestore, getDocs, collection, query, where} from "firebase/firestore";
 
 export default function ItemListContainer (categoryname) {
     const [data, setData] = useState([]);
 
     useEffect(() => {
         const db = getFirestore()
-        const productsRef = collection(db, "productos")
-        getDocs(productsRef).then(snapshot => {
+        const q = query(
+            collection(db, "productos"),
+            where("category", "==", categoryname.categoryname )
+        )
+        getDocs(q).then(snapshot => {
             if(snapshot.size === 0){
                 console.log("No hay resultados")
             }
             const prods = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
-            setData(prods.filter(x => x.category === categoryname.categoryname))
+            setData(prods)
         })
     }, [categoryname])
 
-    /*axios.get(`https://apimocha.com/nbastore/products`).then((res) => {
-            setData(res.data.filter(x => x.category === categoryname.categoryname))
-        })
-
-    */
-    
     return (
         <div>
             <ItemList prods={data} />
